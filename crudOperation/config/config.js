2a6cea2e07c6c2effbbe4b5f8aec70cf46c769fb
Kalyan_VijayaSri <geetkalyan5@gmail.com>
2019-02-02 16:26:40 +0530

    [....coonfig]

--- crudOperation/config/config.js
+++ crudOperation/config/config.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://18.216.164.233/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
